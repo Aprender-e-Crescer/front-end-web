0bb7c6a83e1e0d9e8cdcc78c663ca794dfa53e7c
LucasTheLucas <131563587+LucasTheLucas@users.noreply.github.com>
2023-09-19 20:23:16 -0300

    Teste

--- src/App.tsx
+++ src/App.tsx
@@ -19,10 +19,11 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <h1>Teste</h1>
       </div>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   );
 }
-
+//teste
 export default App;
