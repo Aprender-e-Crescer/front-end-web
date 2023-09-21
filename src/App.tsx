d66675cac8ca605d8e8c351bfc5005c7ff0361f9
LucasTheLucas <131563587+LucasTheLucas@users.noreply.github.com>
2023-09-21 18:02:57 -0300

    Começo do formulario de edição!

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,19 @@ import { useState } from 'react';
 function App() {
 
   return (
-    <div><h1>Lucas Esteve Aqui</h1></div>
+    <>
+      <MinhaPergunta />
+    </>
   );
 }
 //teste
 export default App;
+
+function MinhaPergunta(){
+  return(
+    <div className='bg-blue-200 flex flex-col w-fit text-center font-bold rounded-lg py-4 px-4'>
+      <h1>Digite sua pergunta:</h1>
+      <input className='rounded-full' type="text" />
+    </div>
+    )
+}
