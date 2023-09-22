8171d1af10415cd2f699b5f5d8267b2e963ed722
LucasTheLucas <131563587+LucasTheLucas@users.noreply.github.com>
2023-09-21 21:51:32 -0300

    Ta funcionando Legal

--- src/App.tsx
+++ src/App.tsx
@@ -12,14 +12,14 @@ function App() {
   };
 
   return (
-    <div>
+    <div className="flex flex-col gap-4 justify-items-center items-center py-5">
       {inputs.map(({ name }) => (
-        <div className="bg-orange-500 rounded-lg text-center">
-          <h1>Crie uma pergunta</h1>
+        <div className="bg-orange-500 rounded-lg text-center w-fit flex flex-col gap-2 px-4 py-1">
+          <h1 className="font-bold">Crie ou altere uma pergunta:</h1>
           <input className="rounded-full" type="text" placeholder={name} />
         </div>
       ))}
-      <button onClick={handleClickAddInput} type="button" className="rounded-full bg-blue-500 border-r-8">
+      <button onClick={handleClickAddInput} type="button" className="rounded-full bg-blue-500 w-fit">
         +
       </button>
     </div>
