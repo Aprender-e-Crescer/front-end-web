6b7fc83ab4253629db14973d13cdc586c7a7370d
LucasTheLucas <131563587+LucasTheLucas@users.noreply.github.com>
2023-09-21 21:07:56 -0300

    Adição de perguntas

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
 import { useState } from 'react';
 
+interface IInput {
+  name: string;
+}
+
 function App() {
-  const [perguntaInput, SetperguntaInput] = useState([])
+  const [inputs, setInputs] = useState<IInput[]>([]);
+
+  const handleClickAddInput = () => {
+    setInputs(oldInputs => [...oldInputs, { name: 'pergunta anterior' }]);
+  };
 
   return (
-    <>
-
-      <button className='rounded-full bg-blue-500 border-r-8'>+</button>
-    </>
+    <div>
+      {inputs.map(({ name }) => (
+        <div className="bg-orange-500 rounded-lg text-center">
+          <h1>Crie uma pergunta</h1>
+          <input className="rounded-full" type="text" placeholder={name} />
+        </div>
+      ))}
+      <button onClick={handleClickAddInput} type="button" className="rounded-full bg-blue-500 border-r-8">
+        +
+      </button>
+    </div>
   );
 }
 
-
 export default App;
-
-function perguntaRefresh({perguntaInput})
-{
-  perguntaInput.map(() => {
-    <div className='bg-orange-500'>
-      <h1>
-        SLA
-      </h1>
-    </div>
-  })
-}
-
-function AddPergunta(SetperguntaInput, perguntaInput){ 
-  SetperguntaInput([...perguntaInput,1])
-}
-
-
-
-
-
