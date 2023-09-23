34837e74c058c9d4b7718585919782534c100bac
Joel Cruz <jocruz2021joel@gmail.com>
2023-09-23 17:23:20 -0300

    botão arredondado

--- src/App.tsx
+++ src/App.tsx
@@ -18,10 +18,11 @@ function App() {
           <h1 className="font-bold">Crie ou altere uma pergunta:</h1>
           <input className="rounded-full" type="text" placeholder={name} />
         </div>
-      ))}
-      <button onClick={handleClickAddInput} type="button" className="rounded-full bg-blue-500 w-fit">
-        +
-      </button>
+      ))}    
+        <button onClick={handleClickAddInput} type="button" className="w-6 h-6 rounded-full bg-blue-500 text-white flex items-center justify-center">
+          <span className="text-center">+</span>
+        </button>
+
     </div>
   );
 }
