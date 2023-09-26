010dd11e853dbd4b68d4dcdabad5e63071d11f76
evandro pereira <124467169+vandaosz@users.noreply.github.com>
2023-09-26 20:35:41 -0300

    feature/form

---
+++ src/main.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable prettier/prettier */
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import App from './App.jsx'
+
+
+import './index.css'
+
+import { Formulario } from './routes/form/Formulario.jsx'
+ 
+const router = createBrowserRouter([
+  {
+    // eslint-disable-next-line react/jsx-filename-extension
+    element: <App />,
+    children: [
+      { 
+        path: "/form",
+        element: <Formulario />
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    
+      <RouterProvider router={router}/>
+    
+  </React.StrictMode>
+)
