454d7b9922b6f472932ff4142708e051ac0f0d72
Domini Eduardo <139512722+DominiEduard0@users.noreply.github.com>
2023-09-27 17:52:28 -0300

    Add files via upload
    Div's text

--- App.tsx
+++ App.tsx
@@ -1,78 +1,53 @@
-// Carousel.js
 import React from 'react';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
-import { useStore } from './store.js';
-const Carousel = () => {
-  const store = useStore();
+import { create } from 'zustand';
+
+const useStore = create((set) => ({
+  divSizes: {
+    main: 'full',
+    side: '1/2',
+  },
+}));
+
+const validationSchema = Yup.object().shape({
+});
+
+const App = () => {
+  const { divSizes } = useStore();
+
+  const widthOfMainDiv = `calc(2 * ${divSizes.side})`;
 
   const formik = useFormik({
-    initialValues: {
-      text: store.text,
-      image: store.image,
-    },
-    validationSchema: Yup.object({
-      text: Yup.string().required('Campo obrigatório'),
-      image: Yup.string().required('Campo obrigatório'),
-    }),
+    initialValues: {},
+    validationSchema,
     onSubmit: (values) => {
-      store.updateText(values.text);
-      store.updateImage(values.image);
     },
   });
 
   return (
-    <div className="flex items-center">
-      <div className="w-1/2 p-4">
-        <form onSubmit={formik.handleSubmit}>
-          <div className="mb-4">
-            <label htmlFor="text" className="block text-sm font-medium text-gray-600">
-              Texto:
-            </label>
-            <input
-              type="text"
-              id="text"
-              name="text"
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.text}
-              className="mt-1 p-2 w-full border rounded-md"
-            />
-            {formik.touched.text && formik.errors.text && (
-              <div className="text-red-500 text-sm">{formik.errors.text}</div>
-            )}
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <div style={{ width: '25%', maxWidth: 'screen-lg' }}>
+
+        <div style={{ width: widthOfMainDiv, backgroundColor: 'blue', padding: '16px', marginBottom: '16px' }}>
+          <h1>Div Maior</h1>
+          <p>Texto dentro da div maior...</p>
+        </div>
+
+        <div style={{ display: 'flex' }}>
+          <div style={{ width: divSizes.side, backgroundColor: 'green', padding: '16px', marginRight: '8px' }}>
+            <h2>Div Menor 1</h2>
+            <p>Texto dentro da div menor 1...</p>
           </div>
-          <div className="mb-4">
-            <label htmlFor="image" className="block text-sm font-medium text-gray-600">
-              URL da Imagem:
-            </label>
-            <input
-              type="text"
-              id="image"
-              name="image"
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.image}
-              className="mt-1 p-2 w-full border rounded-md"
-            />
-            {formik.touched.image && formik.errors.image && (
-              <div className="text-red-500 text-sm">{formik.errors.image}</div>
-            )}
+
+          <div style={{ width: divSizes.side, backgroundColor: 'red', padding: '16px', marginLeft: '8px' }}>
+            <h2>Div Menor 2</h2>
+            <p>Texto dentro da div menor 2...</p>
           </div>
-          <button
-            type="submit"
-            className="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600"
-          >
-            Atualizar
-          </button>
-        </form>
-      </div>
-      <div className="w-1/2">
-        <img src={store.image} alt="Imagem" className="w-full h-auto" />
-        <p className="text-xl mt-4">{store.text}</p>
+        </div>
       </div>
     </div>
   );
 };
 
-export default Carousel;
+export default App;
