fcaa416bf9ab71c23449772ada382dee859227a6
LucasTheLucas <131563587+LucasTheLucas@users.noreply.github.com>
2023-09-28 20:19:36 -0300

    Lucas OldCode

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,15 @@
 import { useState } from 'react';
 
 interface IInput {
-  name: string;
   tipodepergunta: string;
   opcoes: string[];
-}
-interface IOption {
-  name: string;
 }
 
 function App() {
   const [inputs, setInputs] = useState<IInput[]>([]);
-  const []
 
   const handleClickAddInput = () => {
-    setInputs(oldInputs => [...oldInputs, { name: 'Espaço para nova pergunta', tipodepergunta: 'text', opcoes: [] }]);
+    setInputs(oldInputs => [...oldInputs, { tipodepergunta: 'text', opcoes: [] }]);
   };
 
   const atualizarInputs = (index: number, value: string) => {
@@ -34,13 +29,12 @@ function App() {
   return (
     <div>
       <div className="bg-gray-200 flex flex-col gap-4 justify-items-center items-center py-5 min-h-screen">
-        {inputs.map(({ name, tipodepergunta, opcoes }, index) => (
+        {inputs.map(({ tipodepergunta }, index) => (
           <div className="bg-white rounded-lg text-center w-fit flex flex-col gap-2 px-4 py-1">
             <h1 className="font-bold">Crie ou altere uma pergunta:</h1>
-            <div className="flex flex-row gap-2">
-              <input className="rounded-full bg-blue-700" type="text" placeholder={name} />
+            <div className="flex flex-row">
               <select
-                className="rounded-full bg-orange-500 font-bold border-none hover:border-none"
+                className="rounded-l-full bg-orange-500 font-bold border-none hover:border-none"
                 onChange={e => atualizarInputs(index, e.target.value)}
                 value={tipodepergunta}
               >
@@ -48,13 +42,17 @@ function App() {
                 <option value="Check">CheckBox</option>
                 <option value="Data">Data</option>
               </select>
+              <input
+                className="rounded-r-full bg-blue-700 placeholder:text-white font-bold"
+                type="text"
+                placeholder="Sua pergunta"
+              />
             </div>
-            {tipodepergunta === 'Check' &&
-              opcoes.map(() => (
-                <div>
-                  <h1>Lucas</h1>
-                </div>
-              ))}
+            {tipodepergunta === 'Check' && (
+              <div>
+                <h1>Luccas</h1>
+              </div>
+            )}
             <button type="button" onClick={() => apagarPergunta(index)} className="text-red-500 font-bold">
               Apagar
             </button>
@@ -73,4 +71,3 @@ function App() {
 }
 
 export default App;
-//Preciso adicionar uma função para adicionar opçoes ao meu checkbox...
