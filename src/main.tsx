fea6892b25321d39c5982a0381c830033aa897fd
alexandre1921 <alexandre_leonardo2010@hotmail.com>
2023-10-10 18:38:53 -0300

    chore(routes): update routes

--- src/main.tsx
+++ src/main.tsx
@@ -17,7 +17,7 @@ const router = createBrowserRouter([
     element: <App />,
     children: [
       {
-        path: '/a-c-landing-page',
+        path: '/',
         element: <ACLandingPage />
       },
       {
