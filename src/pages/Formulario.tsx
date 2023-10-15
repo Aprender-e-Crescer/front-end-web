e64593654faea2624f9c86a3cfaadac7569f3c75
evandro pereira <124467169+vandaosz@users.noreply.github.com>
2023-10-15 15:16:49 -0300

    modal-submit

--- src/pages/Formulario.tsx
+++ src/pages/Formulario.tsx
@@ -1,7 +1,7 @@
+/* eslint-disable import/order */
+import React, { useState, useEffect } from 'react';
 import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
-import { pt } from 'yup-locale-pt';
-import { useState, useEffect } from 'react';
 import Input from '../components/Input/Input';
 import InputDrop from '../components/inputDrop/InputDrop';
 import { HTTP } from '../services/api';
@@ -9,11 +9,22 @@ import { FooterFront } from '../components/FooterFront';
 import headerData from '../data/header.json';
 import footerData from '../data/footer.json';
 import { HeaderFront } from '../components/HeaderFront';
+import { Button, Modal, Spinner } from 'flowbite-react';
+import { useNavigate } from 'react-router-dom';
+import { pt } from 'yup-locale-pt';
 
 Yup.setLocale(pt);
+
 export function Formulario() {
-  // TODO - create type for response data
+  const navigate = useNavigate();
+  function redirect() {
+    navigate('/');
+    console.log('oi');
+  }
   const [data, setData] = useState([]);
+  const [openModal, setOpenModal] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const props = { openModal, setOpenModal };
 
   const getData = async () => {
     try {
@@ -21,7 +32,6 @@ export function Formulario() {
       const result = response.data;
       setData(result);
     } catch (error) {
-      // eslint-disable-next-line no-console
       console.log(error);
     }
   };
@@ -41,20 +51,28 @@ export function Formulario() {
     }, {}),
   );
 
-  const initialValues: { [key: string]: string } = {};
+  const initialValues = {};
+  const validationSchema = {};
 
   fieldsWithName.forEach(field => {
     initialValues[field.name] = '';
+    validationSchema[field.name] = field.required ? Yup.string().required('Campo obrigatório') : Yup.string();
   });
 
-  const handleSubmit = async (values: typeof initialValues) => {
-    try {
-      await HTTP.post('salva/dados', values);
+  const handleSubmit = async values => {
+    const isFormValid = fieldsWithName.every(field => !field.required || !!values[field.name]);
 
-      getData();
-    } catch (error) {
-      // eslint-disable-next-line no-console
-      console.error('Erro ao salvar os dados:', error);
+    if (isFormValid) {
+      try {
+        await HTTP.post('salva/dados', values);
+        getData();
+        setIsLoading(false);
+        setOpenModal('pop-up');
+      } catch (error) {
+        console.error('Erro ao salvar os dados:', error);
+        setIsLoading(false);
+        setOpenModal('pop-up-error');
+      }
     }
   };
 
@@ -64,7 +82,7 @@ export function Formulario() {
       <div className="w-full md:w-[80%] self-center">
         <section className="relative transition-transform duration-[0.3s] ease-[ease-in-out] mx-auto my-0">
           <Formik initialValues={initialValues} onSubmit={handleSubmit} validationSchema={schema}>
-            {({ errors, touched }) => (
+            {({ errors, touched, isValid }) => (
               <Form
                 style={{ fontFamily: 'Arial, sans-serif' }}
                 className="flex flex-col bg-[#f7f7f7] border m-auto p-5 rounded-[10px] border-solid border-[#ccc]"
@@ -115,15 +133,73 @@ export function Formulario() {
                   <button
                     className="bg-[#007bff] text-[white] cursor-pointer px-5 py-2.5 rounded-[10px] border-[none] hover:bg-[#267edd]"
                     type="submit"
+                    disabled={!isValid}
                   >
                     Enviar
                   </button>
                 </div>
+                <Spinner aria-label="Default status example" />;
               </Form>
             )}
           </Formik>
         </section>
       </div>
+      <Modal
+        show={props.openModal === 'pop-up'}
+        size="md"
+        popup
+        onClose={() => {
+          props.setOpenModal(undefined);
+          navigate('/');
+        }}
+      >
+        <Modal.Header />
+        <Modal.Body>
+          <div className="text-center">
+            <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+              Formulário enviado com sucesso!
+            </h3>
+            <div className="flex justify-center gap-4">
+              <Button
+                color="success"
+                onClick={() => {
+                  props.setOpenModal(undefined);
+                  redirect();
+                }}
+              >
+                OK
+              </Button>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
+      <Modal
+        show={props.openModal === 'pop-up-error'}
+        size="md"
+        popup
+        onClose={() => {
+          props.setOpenModal(undefined);
+          navigate('/');
+        }}
+      >
+        <Modal.Header />
+        <Modal.Body>
+          <div className="text-center">
+            <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Erro ao enviar formulário</h3>
+            <div className="flex justify-center gap-4">
+              <Button
+                color="failure"
+                onClick={() => {
+                  props.setOpenModal(undefined);
+                  redirect();
+                }}
+              >
+                OK
+              </Button>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
       <FooterFront
         leftItems={footerData.leftItems}
         rightItems={footerData.rightItems}
