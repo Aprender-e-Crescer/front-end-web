077a336d9b01880b437a29fc54c6118e584b97f5
lucasvazques <gabrielmoreira2128@gmail.com>
2023-10-16 19:59:16 -0300

    card and button previous and next

--- src/pages/AnswerViewer.tsx
+++ src/pages/AnswerViewer.tsx
@@ -20,24 +20,45 @@ export function AnswerViewer() {
         {pageData.map(({ type, data }) => (
           <Switch>
             <Case condition={type === 'ShortAnswersViewer'}>
-              <ShortAnswerViewer {...data} />
+              <Card>
+                <ShortAnswerViewer {...data} />
+              </Card>
             </Case>
           </Switch>
         ))}
         {pageData.map(({ type, data }) => (
           <Switch>
             <Case condition={type === 'DateSelectViewer'}>
-              <DateSelectViewer {...data} />
+              <Card>
+                <DateSelectViewer {...data} />
+              </Card>
             </Case>
           </Switch>
         ))}
         {pageData.map(({ type, data }) => (
           <Switch>
             <Case condition={type === 'Chart'}>
-              <Chart {...data} />
+              <Card>
+                <Chart {...data} />
+              </Card>
             </Case>
           </Switch>
         ))}
+
+        <div className="flex">
+          <a
+            href="#"
+            className="flex items-center justify-center px-3 h-8 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          >
+            Previous
+          </a>
+          <a
+            href="#"
+            className="flex items-center justify-center px-3 h-8 ml-3 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          >
+            Next
+          </a>
+        </div>
       </div>
       <FooterFront
         leftItems={footerData.leftItems}
