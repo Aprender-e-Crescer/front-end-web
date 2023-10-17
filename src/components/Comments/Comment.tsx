1f802427d0cf26591eada3df99ef02100fdf1bf9
Berga Programs <felipefavero772@gmail.com>
2023-10-16 22:04:40 -0300

    .

--- src/components/Comments/Comment.tsx
+++ src/components/Comments/Comment.tsx
@@ -12,21 +12,20 @@ function Comment() {
   return (
     <section className="bg-transparen p-16">
       <div className="max-w-[1000px] h-automy-8 py-0 mx-auto px-4">
-        <h2 className="text-3xl font-semibold text-center mb-2">Comentários</h2>
-        <div className="w-full sm:w-1/2 mx-auto">
-          <Carousel className="h-96" leftControl={<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="white" className="w-10 h-10 bg-gray-700 rounded-full">
+        <div className="w-full  mx-auto">
+          <Carousel className="h-96" leftControl={<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="white" className="w-10 h-10 bg-gray-300 rounded-full">
   <path strokeLinecap="round" strokeLinejoin="round" d="M12.25 9l-3 3m0 0l3 3m-3-3h7.5" />
 </svg>
 
 
 
 }
-rightControl={<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="white" className="w-10 h-10 bg-gray-700 rounded-full">
+rightControl={<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="white" className="w-10 h-10 bg-gray-300 rounded-full">
 <path strokeLinecap="round" strokeLinejoin="round" d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5" />
 </svg>
 
 
-}>
+}> 
             {data
               .find(item => item.type === 'carrousel-testimony')
               ?.content.map(({ image, text, who_is: whoIs }: Comment) => (
